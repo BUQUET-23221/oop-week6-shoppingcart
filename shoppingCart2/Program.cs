6b1a97c9c98a4efcc3eccd6ae95c2adb02168c4f
Sebastien BUQUET <sebastien.buquet@edu.devinci.fr>
2020-10-19 16:20:09 +0200

    Ajoutez des fichiers projet.

---
+++ shoppingCart2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shoppingCart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //orange = 60;
+            //apple = 40;
+
+            List<string> fruits = new List<string>();
+            fruits.Add("Orange");
+            fruits.Add("Orange");
+            fruits.Add("Apple");
+            fruits.Add("Orange");
+
+            Console.WriteLine("Shopping List:");
+            for(int i=0;i<fruits.Count;i++)
+            {
+                Console.WriteLine(fruits[i]);
+            }
+            Console.WriteLine($"You need to pay £{Sum(fruits)}");
+            Console.ReadKey();
+        }
+
+        static int Sum(List<string> fruits)
+        {
+            int sum = 0;
+            for(int i=0;i<fruits.Count;i++)
+            {
+                if(fruits[i]=="Orange")
+                {
+                    sum = sum + 60;
+                }
+                else
+                {
+                    sum = sum + 40;
+                }
+            }
+            return sum/100;
+            
+        }
+
+
+    }
+}
